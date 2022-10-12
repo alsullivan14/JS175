6eb21edc401de916c5af0847a7d371a504912a7e
Adam Sullivan <AdamSullivan500@gmail.com>
2022-10-12 16:43:40 +0000

    Add projects from Networked Applications in JS

---
+++ wine_cellar.js
@@ -0,0 +1,19 @@
+
+
+
+class wineCellar {
+  constructor(name, location) {
+    this.wineList = [];
+    this.wineCount = this.wineList.length;
+    this.name = name;
+    this.location = location;
+  }
+  
+  addWine() {}
+
+  displayAll() {}
+  
+  displayWhites() {}
+  
+  displayReds() {}
+}
